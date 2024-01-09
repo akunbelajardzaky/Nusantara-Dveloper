7155b3c01061b13efc87ce57ad56a11aef0ac1f6
Zeeta:) <ebdtv4@gmail.com>
2024-01-10 04:49:13 +0700

    commit

--- src/views/ServicesView.vue
+++ src/views/ServicesView.vue
@@ -1,3 +1,53 @@
 <template>
-    
+  <!-- FAQ -->
+  <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto mt-12">
+    <!-- Title -->
+    <div class="max-w-3xl mx-auto text-center mb-10 lg:mb-14">
+      <h2 class="text-4xl leading-normal font-bold md:text-3xl md:leading-tight xl:text-6xl text-gray-800 dark:text-gray-200 mb-16 xl:mb-24">Frequently Asked Questions</h2>
+    </div>
+    <!-- End Title -->
+
+    <div class="max-w-5xl mx-auto">
+      <!-- Grid -->
+      <div class="grid sm:grid-cols-2 gap-6 md:gap-12">
+        <div>
+          <h3 class="text-lg font-semibold text-gray-800 dark:text-gray-200">Can I cancel at anytime?</h3>
+          <p class="mt-2 text-gray-600 dark:text-gray-400">Yes, you can cancel anytime no questions are asked while you cancel but we would highly appreciate if you will give us some feedback.</p>
+        </div>
+        <!-- End Col -->
+
+        <div class="text-end mt-20 xl:mt-0">
+          <h3 class="text-lg font-semibold text-gray-800 dark:text-gray-200">My team has credits. How do we use them?</h3>
+          <p class="mt-2 text-gray-600 dark:text-gray-400">Once your team signs up for a subscription plan. This is where we sit down, grab a cup of coffee and dial in the details.</p>
+        </div>
+        <!-- End Col -->
+
+        <div class="mt-20 xl:mt-0">
+          <h3 class="text-lg font-semibold text-gray-800 dark:text-gray-200">How does Preline's pricing work?</h3>
+          <p class="mt-2 text-gray-600 dark:text-gray-400">Our subscriptions are tiered. Understanding the task at hand and ironing out the wrinkles is key.</p>
+        </div>
+        <!-- End Col -->
+
+        <div class="text-end mt-20 xl:mt-0">
+          <h3 class="text-lg font-semibold text-gray-800 dark:text-gray-200">How secure is Preline?</h3>
+          <p class="mt-2 text-gray-600 dark:text-gray-400">Protecting the data you trust to Preline is our first priority. This part is really crucial in keeping the project in line to completion.</p>
+        </div>
+        <!-- End Col -->
+
+        <div class="mt-20 xl:mt-0">
+          <h3 class="text-lg font-semibold text-gray-800 dark:text-gray-200">Do you offer discounts?</h3>
+          <p class="mt-2 text-gray-600 dark:text-gray-400">We've built in discounts at each tier for teams. The time has come to bring those ideas and plans to life.</p>
+        </div>
+        <!-- End Col -->
+
+        <div class="text-end mt-20 xl:mt-0">
+          <h3 class="text-lg font-semibold text-gray-800 dark:text-gray-200">What is your refund policy?</h3>
+          <p class="mt-2 text-gray-600 dark:text-gray-400">We offer refunds. We aim high at being focused on building relationships with our clients and community.</p>
+        </div>
+        <!-- End Col -->
+      </div>
+      <!-- End Grid -->
+    </div>
+  </div>
+  <!-- End FAQ -->
 </template>
